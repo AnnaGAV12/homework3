f1d4c7c1e3dce9a4b9d53aac662cd854efc73f4d
Anna <ds_anna@bk.ru>
2022-05-25 10:45:52 +0300

    Показать кубы чисел, заканчивающихся на четную цифру

---
+++ task6/Program.cs
@@ -0,0 +1,11 @@
+﻿// Показать кубы чисел, заканчивающихся на четную цифру
+Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+if (num % 2 == 0) 
+{
+    Console.WriteLine($"Куб числа {num} равен {num * num * num}");
+}
+else 
+{
+    Console.WriteLine($"Число {num} не заканчивается на четную цифру");
+}
